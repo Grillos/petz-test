1f2dafc84b2c1a4a2826eb4a2d7792cb6c08435f
cbyk-dev <robsongrillo13@gmail.com>
2020-04-07 16:48:07 -0300

    feat(test): Include mockMvc test controller

--- src/test/java/com/petz/test/ApplicationTests.java
+++ src/test/java/com/petz/test/ApplicationTests.java
@@ -1,10 +1,9 @@
-package com.petz.test.security;
+package com.petz.test;
 
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.runner.RunWith;
 
